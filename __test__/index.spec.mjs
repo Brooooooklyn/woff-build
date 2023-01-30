9a33e7ce98660dedb08a7035241f2370e188e81d
一丝 <percyley@qq.com>
2023-01-30 23:35:34 +0800

    test: add ttf2woff2 module to compare buffer length

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -4,10 +4,16 @@ import { fileURLToPath } from 'url'
 
 import test from 'ava'
 
+import wawoff from 'wawoff2'
 import { convertTTFToWOFF2 } from '../index.js'
 
 const fixture = await readFile(join(fileURLToPath(import.meta.url), '..', './iconsfont.ttf'))
 
 test('should be able to convert ttf to woff2', async (t) => {
   t.notThrows(() => convertTTFToWOFF2(fixture))
+})
+
+test('should be same length as the wawoff2', async (t) => {
+  const wawoffResult = await wawoff.compress(fixture)
+  t.is(convertTTFToWOFF2(fixture).length, wawoffResult.length)
 })
